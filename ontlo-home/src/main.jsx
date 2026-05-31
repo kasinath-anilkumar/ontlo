5b1709c3e0ccf7531b1ceb23cf9107c434d37caa
kasinath <kasinathanikumar@gmail.com>
2026-05-31 13:24:27 +0530

    feat: initialize project with React, Tailwind CSS, and Inter font configuration

--- ontlo-home/src/main.jsx
+++ ontlo-home/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/tailwind.css'
 import './styles/globals.css'
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
